f0298655a11dd4ed67842d8a441393506e952833
James Bond <SupawiriyaSaranarak@github.com>
2022-10-27 20:59:17 +0700

    2 commit

--- java/code/Start.java
+++ java/code/Start.java
@@ -1,6 +1,26 @@
 package code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 class Start {
     public static void main (String[] data){
         System.out.println("Welcome to java-27");
+        ApplicationContext context;
+        context = SpringApplication.run(Setup.class);
+    }
+}
+@SpringBootApplication
+class Setup {
+    
+}
+@RestController
+class Sample {
+    @RequestMapping("/check")
+    int check() {
+        return 321;
     }
 }
