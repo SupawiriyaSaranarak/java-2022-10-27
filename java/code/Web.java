445bd49c7fd778d15d091dfb9139c339364eac69
James Bond <SupawiriyaSaranarak@github.com>
2022-10-28 21:30:35 +0700

    3rd commit

--- java/code/Web.java
+++ java/code/Web.java
@@ -5,15 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller // return string name of html // use to make MVC web application
+@Controller // return string name of html // use to make MVC web application //access html file in templates folder
 class Web {
+    
     @RequestMapping("/") 
     String showHome (Model model) {
         model.addAttribute("brand", "Star Coffee");
         String[] data = {"Latte", "Matcha", "Cappuccino"};
         model.addAttribute("products", data);
         model.addAttribute("telephone", "022222222");
-        return "report"; //report.html
+        return "newhtml"; //report.html in templates folder
+    }
+    
+    @RequestMapping("/icoffee") 
+    String showICoffee (Model model) {
+        model.addAttribute("brand", "Star Coffee");
+        String[] data = {"Latte", "Matcha", "Cappuccino"};
+        model.addAttribute("products", data);
+        model.addAttribute("telephone", "022222222");
+        return "report"; //report.html in templates folder
     }
     
     @RequestMapping("/sample")
@@ -25,3 +35,17 @@ class Web {
 }
 
 record Player(String name, int number) {}
+
+class Product {
+    double price;
+    double getSpecialPrice () {
+        return price * 0.95;
+    }
+}
+
+class Book extends Product {
+    @Override // additional keyword for check the method in superclass // if not have this annotation it can override the same way // but it help programmer to ensure that it override correctly if not it will show error
+    double getSpecialPrice () {
+        return price * 0.9;
+    }
+}
